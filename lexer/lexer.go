8f18ab184b4cd58c51c55c495fe146174e2ab558
Raf-Batista <RafaelB2424@gmail.com>
2024-05-03 21:52:41 +0000

    Add function to read characters of input string

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -9,5 +9,18 @@ type Lexer struct {
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
+}
+
+// returns next character and advance pos in input string
+func (l *Lexer) readChar() {
+	// check if end of input reached
+	if l.readPosition >= len(l.input) {
+		l.ch = 0 // ASCII code for "NUL", haven't read anything or EOF
+	} else { // haven't reached end, so l.ch to next character
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition // points to pos we last read
+	l.readPosition += 1         // always points to position which will be read next
 }
